0c1c847b5b4ee0335e4e4be1cd01c4b1b49b8f10
takumi-saito <takumi.saitoh.1992@gmail.com>
2023-05-29 04:37:58 +0900

    update java 8 -> 17 and github actions

---
+++ app/build.gradle.kts
@@ -0,0 +1,135 @@
+import org.gradle.testing.jacoco.tasks.JacocoReport
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    id("com.google.firebase.firebase-perf")
+    id("com.google.dagger.hilt.android")
+    id("jacoco")
+}
+
+android {
+    namespace ="com.kireaji.minimallauncherapp"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.kireaji.minimallauncherapp"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 120
+        versionName = "1.2.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled =  false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.5"
+    }
+}
+
+dependencies {
+
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
+    implementation ("androidx.core:core-ktx:1.8.0")
+    implementation (platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation ("androidx.activity:activity-compose:1.5.1")
+    implementation (platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-graphics")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material3:material3")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest")
+    implementation ("com.google.accompanist:accompanist-drawablepainter:0.28.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.5")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.fragment:fragment-ktx:1.5.7")
+    implementation ("com.google.android.material:material:1.6.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    // Hilt
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-compiler:2.44")
+
+    implementation (platform("com.google.firebase:firebase-bom:31.2.2"))
+    implementation ("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-crashlytics")
+    implementation ("com.google.firebase:firebase-perf")
+
+    // JUnit
+    testImplementation("junit:junit:4.13.2")
+    // Mockito
+    testImplementation("org.mockito:mockito-inline:3.12.4")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.2.0")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
+}
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    dependsOn("testDevelopDebugUnitTest")
+
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+
+    val excludes = listOf(
+            "**/R.class",
+            "**/R$*.class",
+            "**/BuildConfig.*",
+            "**/Manifest*.*",
+            "**/*Test*.*",
+            "android/**/*.*"
+    )
+    val debugTree = fileTree(
+            mapOf(
+                    "dir" to "$buildDir/tmp/kotlin-classes/developDebug",
+                    "include" to "**/*UseCase.*",
+                    "excludes" to excludes
+            )
+    )
+    val mainSrc = "${project.projectDir}/src/main/java"
+
+    sourceDirectories.setFrom(files(mainSrc))
+    classDirectories.setFrom(files(debugTree))
+    executionData.setFrom(fileTree(mapOf("dir" to "$buildDir", "includes" to listOf("jacoco/testDevelopDebugUnitTest.exec"))))
+}

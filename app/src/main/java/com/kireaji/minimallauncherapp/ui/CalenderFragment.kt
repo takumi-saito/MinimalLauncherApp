62e94358c6c30118ad912002c97f8b20e30fbf42
Takumi Saito <t-saito@TakuminoMac-mini.local>
2025-11-30 04:51:13 +0900

    ref

--- app/src/main/java/com/kireaji/minimallauncherapp/ui/CalenderFragment.kt
+++ app/src/main/java/com/kireaji/minimallauncherapp/ui/CalenderFragment.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.viewModels
-import com.kireaji.minimallauncherapp.R
 import com.kireaji.minimallauncherapp.ui.compose.CalendarScreen
 import com.kireaji.minimallauncherapp.ui.viewmodel.CalendarViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,20 +29,18 @@ class CalenderFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.fragment_calender, container, false)
-        root.findViewById<ComposeView>(R.id.compose_view).apply {
-            setContent {
-                CalendarScreen(viewModel)
-            }
-        }
-
+    ): View {
         activity?.registerReceiver(dateTimeChangedReceiver, IntentFilter().also {
             it.addAction(Intent.ACTION_TIME_TICK)
             it.addAction(Intent.ACTION_TIMEZONE_CHANGED)
             it.addAction(Intent.ACTION_TIME_CHANGED)
         })
-        return root
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                CalendarScreen(viewModel)
+            }
+        }
     }
 
     override fun onDestroyView() {
